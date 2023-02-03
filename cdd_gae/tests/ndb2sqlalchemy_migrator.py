02e43c58b69483f6feeea7f986ed447d25d45a49
skushnir123 <samkushnir@gmail.com>
2023-02-03 15:40:53 -0500

    Update ndb2sqlalchemy_migrator.py

--- cdd_gae/tests/ndb2sqlalchemy_migrator.py
+++ cdd_gae/tests/ndb2sqlalchemy_migrator.py
@@ -2,8 +2,15 @@
 Tests for migration scripts from NDB to SQLalchemy
 """
 from unittest import TestCase
-
+from tempfile import mkdtemp
+import os
 class TestNDB2SqlalchemyMigrator(TestCase):
     """
-    Tests whether Parquet to table methods work correctly
+    Tests whether ndb to Sqlalchemy methods work correctly
     """
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        """Construct temporary module for use by tests"""
+        cls.tempdir = mkdtemp()
+        temp_module_dir = os.path.join(cls.tempdir, "gen_test_module")
